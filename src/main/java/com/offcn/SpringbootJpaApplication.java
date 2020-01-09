106613a17435fcc064a094df7d9e81f5ff1ccbf8
xiaoban <test@qq.com>
2020-01-09 20:20:12 +0800

    新增注释

--- src/main/java/com/offcn/SpringbootJpaApplication.java
+++ src/main/java/com/offcn/SpringbootJpaApplication.java
@@ -3,6 +3,9 @@ package com.offcn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 新增注释
+ */
 @SpringBootApplication
 public class SpringbootJpaApplication {
 

e62473a9a94daeaf1c56bab9a6a644382901755f
xiaoban <test@qq.com>
2020-01-09 21:16:05 +0800

    新增支付功能

---
+++ src/main/java/com/offcn/pay/Impl/PayServiceImpl.java
@@ -0,0 +1,10 @@
+package com.offcn.pay.Impl;
+
+import com.offcn.pay.PayService;
+
+public class PayServiceImpl implements PayService {
+    @Override
+    public void add(Long id) {
+        System.out.println("增加成功");
+    }
+}
---
+++ src/main/java/com/offcn/pay/PayService.java
@@ -0,0 +1,5 @@
+package com.offcn.pay;
+
+public interface PayService {
+    public void add(Long id);
+}
